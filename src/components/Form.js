8774c436fc1ed4a1c9ad498b58eacf89d1af1174
BII KIPKOECH COLLINS <ckipkoech17@gmail.com>
2022-07-26 20:39:47 +0300

    commit tree 4c8513b0c2eb85880a7902b1cf549b06eafb4ec7

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,9 +14,9 @@ function Form(props) {
 
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
     </form>
   );
 }
